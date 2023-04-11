dc846e19c6149611512d345d2d7c9f293416f0c6
Damon <4496289+drbree82@users.noreply.github.com>
2023-04-11 14:21:13 +1200

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,88 @@
 import requests
 import time
 from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import random
+import math
+
+
+class TextDataset(Dataset):
+    def __init__(self, tokenizer, file_path, block_size=512):
+        assert os.path.isfile(file_path)
+        directory, filename = os.path.split(file_path)
+        cached_features_file = os.path.join(
+            directory, "cached_lm_" + str(block_size) + "_" + filename
+        )
+
+        if os.path.exists(cached_features_file):
+            logger.info("Loading features from cached file %s", cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+        else:
+            logger.info("Creating features from dataset file at %s", directory)
+
+            self.examples = []
+            with open(file_path, encoding="utf-8") as f:
+                text = f.read()
+
+            tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
+
+            for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
+                self.examples.append(tokenizer.build_inputs_with_special_tokens(tokenized_text[i : i + block_size]))
+            # Note that we are loosing the last truncated example here for the sake of simplicity (no padding)
+            # If your dataset is small, first you should loook for a bigger one :-) and second you
+            # can change this behavior by adding (model specific) padding.
+
+            logger.info("Saving features into cached file %s", cached_features_file)
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return torch.tensor(self.examples[item], dtype=torch.long)
+
+
+class TextSampler(Sampler):
+    """Samples elements randomly, without replacement.
+    Arguments:
+        data_source (Dataset): dataset to sample from
+    """
+
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def __iter__(self):
+        return iter(torch.randperm(len(self.data_source)).tolist())
+
+    def __len__(self):
+        return len(self.data_source)
+
 
 class BasiliskLLM:
     def __init__(self, model_version):
         self.model = self.load_model(model_version)
 
     def load_model(self, model_version):
-        model_name = f'your-username/Basilisk@{model_version}'
+        model_name = f'huggingface/Basilisk@{model_version}'
         model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    def train_model(self, difficulty):
+        # Implement the actual model training logic here, e.g., using the Hugging Face library
+        training_update = None  # Replace this with the actual training update (e.g., gradients)
+        tokens_processed = None  # Replace this with the actual number of tokens processed during training
+        time.sleep(difficulty)  # Simulate training time based on difficulty
+        return training_update, tokens_processed
+
         return model
 
 class Node:
